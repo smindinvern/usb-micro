346bd5ed9f131c9630144b0a9e0b47e597940e15
Nickolas Lloyd <nick.lloyd@qorvo.com>
2018-09-26 14:49:04 -0400

    Flesh out GPIO support for SAMD21 devices

--- include/gpio.hh
+++ include/gpio.hh
@@ -8,35 +8,30 @@ extern "C" {
 #define GPIO_FUNC(x) \
 	xglue(CHIP_FAMILY, x)
 	
-	void GPIO_FUNC(_configure_pin_as_gpio)(unsigned char pin_no);
-	void GPIO_FUNC(_configure_pin_direction)(unsigned char pin_no,
+	void GPIO_FUNC(_configure_pin_as_gpio)(unsigned char pin);
+	void GPIO_FUNC(_configure_pin_direction)(unsigned char pin,
 											 bool output);
-	enum PinStates {
-		PinHighState,
-		PinLowState,
-		PinTriState
-	};
-	void GPIO_FUNC(_set_pin_state)(unsigned char pin_no,
-								   PinStates state);
-	void GPIO_FUNC(_enable_pin_pullup)(unsigned char pin_no);
-	void GPIO_FUNC(_disable_pin_pullup)(unsigned char pin_no);
-	void GPIO_FUNC(_enable_pin_pulldown)(unsigned char pin_no);
-	void GPIO_FUNC(_disable_pin_pulldown)(unsigned char pin_no);
+	void GPIO_FUNC(_set_pin_value)(unsigned char pin,
+								   bool value);
+	void GPIO_FUNC(_tristate_pin)(unsigned char pin);
+	void GPIO_FUNC(_enable_pin_pullup)(unsigned char pin);
+	void GPIO_FUNC(_enable_pin_pulldown)(unsigned char pin);
+	void GPIO_FUNC(_disable_pin_pull)(unsigned char pin);
 
-#define configure_pin_as_gpio(pin_no) \
-	GPIO_FUNC(_configure_pin_as_gpio)(pin_no)
-#define configure_pin_direction(pin_no, output) \
-	GPIO_FUNC(_configure_pin_direction)(pin_no, output)
-#define set_pin_state(pin_no, state) \
-	GPIO_FUNC(_set_pin_state)(pin_no, state)
-#define enable_pin_pullup(pin_no) \
-	GPIO_FUNC(_enable_pin_pullup)(pin_no)
-#define disable_pin_pullup(pin_no) \
-	GPIO_FUNC(_disable_pin_pullup)(pin_no)
-#define enable_pin_pulldown(pin_no) \
-	GPIO_FUNC(_enable_pin_pulldown)(pin_no)
-#define disable_pin_pulldown(pin_no) \
-	GPIO_FUNC(_disable_pin_pulldown)(pin_no)
+#define configure_pin_as_gpio(pin) \
+	GPIO_FUNC(_configure_pin_as_gpio)(pin)
+#define configure_pin_direction(pin, output) \
+	GPIO_FUNC(_configure_pin_direction)(pin, output)
+#define set_pin_value(pin, value) \
+	GPIO_FUNC(_set_pin_value)(pin, value)
+#define tristate_pin(pin) \
+	GPIO_FUNC(_tristate_pin)(pin)
+#define enable_pin_pullup(pin) \
+	GPIO_FUNC(_enable_pin_pullup)(pin)
+#define enable_pin_pulldown(pin) \
+	GPIO_FUNC(_enable_pin_pulldown)(pin)
+#define disable_pin_pull(pin) \
+	GPIO_FUNC(_disable_pin_pullup)(pin)
 	
 #ifdef __cplusplus
 }
